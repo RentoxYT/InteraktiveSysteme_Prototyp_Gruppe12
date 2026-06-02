81270a52c3d8c5ce6534c9f9e8c2dfa8b6520d12
RentoxYT <48733881+RentoxYT@users.noreply.github.com>
2026-06-02 13:17:47 +0200

    Enum

--- prototypMain.ino
+++ prototypMain.ino
@@ -1,14 +1,31 @@
-int ledPin1 =  A2; // the number of the LED pin
-int ledPin2 =  A3;
-int ledPin3 =  A4;
-int ledPin4 =  A5;
-int ledPin5 =  A6;
+int ledPin1 =  2; // the number of the LED pin
+int ledPin2 =  3;
+int ledPin3 =  4;
+int ledPin4 =  5;
+int ledPin5 =  6;
 
-unsigned long Phase1 = 15000; // milliseconds of on-time
-unsigned long Phase2 = 30000; // milliseconds of off-time
-unsigned long Phase3 = 45000; // milliseconds of off-time
-unsigned long Phase4 = 60000; // milliseconds of off-time
-unsigned long Phase5 = 75000; // milliseconds of off-time
+int red = 11;
+int green = 12;
+int blue = 13;
+
+enum STATE {
+  
+  Wait_For_Input,
+
+  User_Infront,
+
+  User_Away,
+
+  End,
+};
+
+STATE currentState = Wait_For_Input;
+
+unsigned long Phase1 = 15000; // milliseconds 
+unsigned long Phase2 = 30000; // milliseconds 
+unsigned long Phase3 = 45000; // milliseconds 
+unsigned long Phase4 = 60000; // milliseconds 
+unsigned long Phase5 = 75000; // milliseconds 
 void setup()
 {
   // set the digital pin as output:
@@ -26,28 +43,42 @@ void setup()
 
 void loop()
 {
-  // check to see if it's time to change the state of the LED
-  unsigned long currentMillis = millis();
+   switch (currentState) { 
+      case Wait_For_Input:
+          break;
+      case User_Infront:
 
-  if(currentMillis >= Phase1)
-  {
-   
-    digitalWrite(ledPin1, HIGH); // Update the actual LED
-  }
-  if(currentMillis >= Phase2)
-  {
-    digitalWrite(ledPin2, HIGH); // Update the actual LED
-  }
-   if(currentMillis >= Phase3)
-  {
-    digitalWrite(ledPin3, HIGH); // Update the actual LED
-  }
-   if(currentMillis >= Phase4)
-  {
-    digitalWrite(ledPin4, HIGH); // Update the actual LED
-  }
-   if(currentMillis >= Phase5)
-  {
-    digitalWrite(ledPin5, HIGH); // Update the actual LED
-  }
+          // check to see if it's time to change the state of the LED
+          unsigned long currentMillis = millis();
+
+          if(currentMillis >= Phase1)
+          {
+          
+            digitalWrite(ledPin1, HIGH); // Update the actual LED
+          }
+          if(currentMillis >= Phase2)
+          {
+            digitalWrite(ledPin2, HIGH); // Update the actual LED
+          }
+          if(currentMillis >= Phase3)
+          {
+            digitalWrite(ledPin3, HIGH); // Update the actual LED
+          }
+          if(currentMillis >= Phase4)
+          {
+            digitalWrite(ledPin4, HIGH); // Update the actual LED
+          }
+          if(currentMillis >= Phase5)
+          {
+            digitalWrite(ledPin5, HIGH); // Update the actual LED
+          }
+          break;
+
+      case User_Away:
+          break;
+      case End:
+          break;
+
+   }
+ 
 }
