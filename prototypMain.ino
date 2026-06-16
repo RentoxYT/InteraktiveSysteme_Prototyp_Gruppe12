fff3ec51c32a9b2b5e5602123090f651d47bd11c
RentoxYT <48733881+RentoxYT@users.noreply.github.com>
2026-06-16 15:59:16 +0200

    Update prototypMain.ino

--- prototypMain.ino
+++ prototypMain.ino
@@ -9,16 +9,23 @@ const int ledPin4 = 5;
 const int ledPin5 = 6;
 
 // Ultraschall
-#define PIN_TRIGGER 7
-#define PIN_ECHO     8
+#define PIN_TRIGGER 9
+#define PIN_ECHO     10
 
 // Sonstiges
 #define BUZZER_PIN 12
 #define BUTTON_PIN 13
 
+int LEDrot = A1;
+
+int LEDgruen = A2;
+
+int LEDblau = A3;
+
+int Helligkeit = 255;  // Zahlenwert zwischen 0 und 255 – gibt die Leuchtstärke der einzelnen Farbe an
 const int SENSOR_MAX_RANGE = 300;
 unsigned long awayStart = 0;
-
+unsigned long returnStart = 0;
 // Servos
 Servo ServomotorRed;
 Servo ServomotorGreen;
@@ -55,9 +62,14 @@ void setup()
 {
     Serial.begin(9600);
     cute.init(BUZZER_PIN);
-    ServomotorGreen.attach(10);
-    ServomotorRed.attach(9);
+    ServomotorGreen.attach(7);
+    ServomotorRed.attach(8);
+  pinMode(LEDblau, OUTPUT);
+ServomotorGreen.write(170);
+ServomotorRed.write(80);
+  pinMode(LEDgruen, OUTPUT);
 
+  pinMode(LEDrot, OUTPUT);
     pinMode(PIN_TRIGGER, OUTPUT);
     pinMode(PIN_ECHO, INPUT);
 
@@ -68,7 +80,6 @@ void setup()
     pinMode(ledPin5, OUTPUT);
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     resetLEDs();
-
     digitalWrite(PIN_TRIGGER, LOW);
 }
 
@@ -76,6 +87,7 @@ void setup()
 
 void loop()
 {
+    measureDistance();
      if (millis() - lastMeasure >= 50)
     {
         lastMeasure = millis();
@@ -89,7 +101,7 @@ void loop()
     // -------------------------
     case WAIT_FOR_INPUT:
 {
-        resetLEDs();
+    resetLEDs();
 
 
     if (digitalRead(BUTTON_PIN) == LOW && distance < 100) // z.B. unter 100 cm
@@ -106,6 +118,7 @@ void loop()
     // -------------------------
    case USER_INFRONT:
 {
+    
     if (distance >= 300)
     {
         if (awayStart == 0)
@@ -113,12 +126,15 @@ void loop()
             awayStart = millis(); // Zeitpunkt merken
         }
 
-        if (millis() - awayStart >= 2000) // 2 Sekunden weg
+        if (millis() - awayStart >= 200) // 2 Sekunden weg
         {
             Serial.println("Benutzer weg.");
-            cute.play(S_SAD);
+            ServomotorGreen.write(170);
+            ServomotorRed.write(80);
+            cute.play(S_CONFUSED);
             awayStart = 0;
             currentState = USER_AWAY;
+            returnStart = 0;
         }
     }
     else
@@ -137,7 +153,7 @@ void loop()
     if (elapsed >= Phase5)
     {
         digitalWrite(ledPin5, HIGH);
-        //cute.play(S_HAPPY);
+        cute.play(S_HAPPY);
         Serial.println("Test Vor Final");
         currentState = FINAL;
         Serial.println(currentState);
@@ -153,25 +169,52 @@ void loop()
     // -------------------------
 case USER_AWAY:
 {
+     ServomotorRed.write(170);
+    
     resetLEDs();
+      analogWrite(LEDgruen, 0); // gruen einschalten
+     analogWrite(LEDrot, Helligkeit); //Rot aus
 
     if (distance < 300)
     {
-        startTime = millis();
-        currentState = USER_INFRONT;
+        if (returnStart == 0)
+        {
+            // Zeitpunkt merken, wann der Benutzer zurückgekommen ist
+            returnStart = millis();
+        }
+
+        // Benutzer war 2 Sekunden durchgehend da
+        if (millis() - returnStart >= 500)
+        {
+            startTime = millis();
+            returnStart = 0;
+            ServomotorGreen.write(170);
+            ServomotorRed.write(80);
+            analogWrite(LEDgruen, 0); // gruen aus
+     analogWrite(LEDrot, 0); //Rot aus
+            currentState = USER_INFRONT;
+        }
+    }
+    else
+    {
+        // Benutzer ist wieder weggegangen -> Zeit zurücksetzen
+        returnStart = 0;
     }
 
     break;
-}  
+}
 case FINAL:
 {
     
+    ServomotorGreen.write(75);
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin3, HIGH);
     digitalWrite(ledPin4, HIGH);
     digitalWrite(ledPin5, HIGH);
 
+    analogWrite(LEDgruen, Helligkeit); // gruen einschalten
+     analogWrite(LEDrot, 0); //Rot aus
     if (digitalRead(BUTTON_PIN) == LOW)
     {
         while(digitalRead(BUTTON_PIN) == LOW) {
@@ -181,6 +224,8 @@ case FINAL:
         resetLEDs();
         awayStart = 0;
         startTime = 0;
+            ServomotorGreen.write(170);
+    ServomotorRed.write(90);
         currentState = WAIT_FOR_INPUT;
     }
 
@@ -215,10 +260,10 @@ void measureDistance()
         distance = durationTime / 58;
     }
 
- /*   Serial.print("Distance to object: ");
+  Serial.print("Distance to object: ");
     Serial.print(distance);
     Serial.println(" cm");
-    Serial.println(currentState); */
+    Serial.println(currentState); 
    
     
 }
@@ -234,4 +279,8 @@ void resetLEDs()
     digitalWrite(ledPin3, LOW);
     digitalWrite(ledPin4, LOW);
     digitalWrite(ledPin5, LOW);
+      analogWrite(LEDgruen, 0); // gruen aus
+     analogWrite(LEDrot, 0); //Rot aus
+
 }
+
